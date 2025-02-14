8b9677f67af29bfcc1704830bbd4cc16dc5f0ccb
Nazar <nick20name17@gmail.com>
2025-02-14 16:03:24 +0200

    first lesson

---
+++ script.js
@@ -0,0 +1,15 @@
+let firstVariable;
+let secondVariable = 20;
+
+const PI = 3.14;
+
+console.log(thirdVariable);
+var thirdVariable = 30;
+
+// console.log(firstVariable * PI);
+
+firstVariable = 1000;
+
+console.log(firstVariable);
+
+// alert('Hello World!');
