2347dfff2141c9161c29453c1aef2f75ee5d55e9
Nazar <nick20name17@gmail.com>
2025-02-17 15:39:38 +0200

    add 3 lesson

---
+++ lesson_03/script.js
@@ -0,0 +1,15 @@
+const colors = ['red', 'green', 'blue'];
+
+console.log(colors.length);
+
+colors[0] = 'pink';
+
+colors.pop();
+colors.shift();
+
+console.log(colors);
+
+colors.push('yellow');
+colors.unshift('purple');
+
+console.log(colors);
