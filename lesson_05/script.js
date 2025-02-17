bc110fb1e7b21d667dba3674f0fd9714bf3aa624
Nazar <nick20name17@gmail.com>
2025-02-17 16:35:08 +0200

    add lesson 05

---
+++ lesson_05/script.js
@@ -0,0 +1,31 @@
+// const a = +prompt('Enter a')
+// const b = +prompt('Enter b')
+
+// const res = parseInt(a) + parseInt(b)
+// const res = a + b
+
+// alert('Sum of a and b: ' + res)
+
+// const isAdult = confirm('Are you 18 or older?')
+
+// if (isAdult) {
+//     alert('You are old enough')
+// } else {
+//     alert('You are not old enough. Go away')
+// }
+
+// const fruit = prompt('Enter a fruit', 'apple')
+
+// switch (fruit) {
+//     case 'apple':
+//         alert('Apple is good for you')
+//         break
+//     case 'banana':
+//         alert('Banana is good for you')
+//         break
+//     case 'orange':
+//         alert('Orange is good for you')
+//         break
+//     default:
+//         alert('Unknown fruit')
+// }
