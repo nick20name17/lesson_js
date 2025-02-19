6ca2cb84af40d62dc80e01caa7688e654a774ea2
Nazar <nick20name17@gmail.com>
2025-02-19 15:29:28 +0200

    add lesson 06

---
+++ lesson_06/script.js
@@ -0,0 +1,19 @@
+let count = 0
+
+while (count < 5) {
+    console.log(count)
+
+    // count += 1 // count = count + 1 - 5 U*
+    count++
+}
+
+const users = ['John', 'Bob', 'Ann', 'Mark', 'Nazar']
+
+// console.log(users[0])
+// console.log(users[1])
+// console.log(users[2])
+// console.log(users[3])
+
+for (let i = 0; i < users.length; i++) {
+    console.log(users[i])
+}
