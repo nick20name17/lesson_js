153b54a2ed4c12020cb1d744d624d22c2b6fcc94
nick20name17 <nick20name17@gmail.com>
2025-03-12 16:30:41 +0200

    add task operations

--- lesson_16/script.js
+++ lesson_16/script.js
@@ -1,32 +1,105 @@
 const form = document.querySelector('.form')
 const taskInput = document.querySelector('#task')
+const tasksList = document.querySelector('.tast-list')
 
-const tasks = [
-    {
-        id: 1,
-        text: 'Do homework',
-        checked: false,
-    },
-]
+const filterButtons = document.querySelectorAll('.filter-btn')
 
-function renderTasks(taskText) {
-    const tasksList = document.querySelector('.tast-list')
+let tasks = []
+let filter = 'all'
 
-    const listItem = document.createElement('li')
-    listItem.innerHTML = `<span>${taskText}</span>`
-    listItem.classList.add('task-item')
+filterButtons.forEach((filterButton) => {
+    filterButton.addEventListener('click', () => {
+        filterButtons.forEach((filterButton) => {
+            filterButton.classList.remove('active')
+        })
+        filterButton.classList.add('active')
 
-    const deleteBtn = document.createElement('button')
-    deleteBtn.textContent = 'Delete'
-    deleteBtn.classList.add('delete-btn')
+        filter = filterButton.id
+    })
+})
 
-    deleteBtn.addEventListener('click', () => {
-        listItem.remove()
+function showEmptyMessage(show) {
+    if (show) {
+        const emptyMessage = document.createElement('li')
+        emptyMessage.classList.add('empty-message')
+        emptyMessage.textContent = 'No tasks added'
+        tasksList.appendChild(emptyMessage)
+    }
+}
+
+function renderTasks() {
+    tasksList.innerHTML = ''
+
+    showEmptyMessage(tasks.length === 0)
+
+    const filteredTasks = tasks.filter((task) => task.completed === false)
+
+    filteredTasks.forEach((task) => {
+        const checkbox = document.createElement('input')
+        checkbox.type = 'checkbox'
+        checkbox.checked = task.completed
+        checkbox.addEventListener('change', () => {
+            toggleCompleted(task.id)
+        })
+
+        const listItem = document.createElement('li')
+        listItem.innerHTML = `<span>${task.text}</span>`
+        listItem.classList.add('task-item')
+
+        if (task.completed) {
+            listItem.classList.add('completed')
+        }
+
+        const deleteBtn = document.createElement('button')
+        deleteBtn.textContent = 'Delete'
+        deleteBtn.classList.add('delete-btn')
+
+        deleteBtn.addEventListener('click', () => {
+            deleteTask(task.id)
+        })
+
+        listItem.appendChild(deleteBtn)
+        listItem.insertAdjacentElement('afterbegin', checkbox)
+
+        tasksList.appendChild(listItem)
+    })
+}
+
+renderTasks()
+
+function addTask(text) {
+    const newTask = {
+        id: tasks.length + 1,
+        text,
+        completed: false,
+    }
+
+    tasks.push(newTask)
+
+    renderTasks()
+}
+
+function deleteTask(taskId) {
+    tasks = tasks.filter((task) => task.id !== taskId)
+    renderTasks()
+}
+
+function toggleCompleted(taskId) {
+    const task = tasks.find((task) => {
+        return task.id === taskId
     })
 
-    listItem.appendChild(deleteBtn)
+    if (task) {
+        // if (task.completed) {
+        //     task.completed = false
+        // } else {
+        //     task.completed = true
+        // }
 
-    tasksList.appendChild(listItem)
+        task.completed = !task.completed
+
+        renderTasks()
+    }
 }
 
 form.addEventListener('submit', (event) => {
@@ -35,7 +108,7 @@ form.addEventListener('submit', (event) => {
     const taskText = taskInput.value.trim()
 
     if (taskText !== '') {
-        renderTasks(taskText)
+        addTask(taskText)
     }
 
     taskInput.value = ''
